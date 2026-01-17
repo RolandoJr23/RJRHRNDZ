a70a44652ec6ae7bd70c4981e67e756d7881b2f2
RolandoJr23 <rolandojrhernandez0623200@gmail.com>
2026-01-17 08:26:14 +0800

    Adding darkmode display

--- Javascript/index.js
+++ Javascript/index.js
@@ -52,6 +52,27 @@ hamMenu.addEventListener("click", () => {
     }
 });
 
+// ************************************Dark Mode************************************
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+if(darkmode === 'active') enableDarkmode()
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    darkmode.style.transition = ".5s ease";
+});
 
 // ************************************Header Quotes, Name and Course Auto Typed**********************
 const typedJS = window.Typed;
--- Javascript/projectdetails.js
+++ Javascript/projectdetails.js
@@ -44,6 +44,28 @@ hamMenu.addEventListener("click", () => {
     }
 });
 
+// ************************************Dark Mode************************************
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', null);
+}
+
+if(darkmode === 'active') enableDarkmode()
+
+themeSwitch.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+    darkmode.style.transition = ".5s ease";
+});
+
 
 // ************************************Display All Projects************************************
 const projects = document.querySelector("#projectbox");
